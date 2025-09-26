4a700a0929b972cfc55a7c26f732eb970dddebe1
Than <thanthemannn@gmail.com>
2025-09-25 22:48:47 -0400

    polished up the visual effects

--- scripts/candle_player/candle_controller.gd
+++ scripts/candle_player/candle_controller.gd
@@ -1,16 +1,11 @@
 extends Node
 
-@export var candle_settings: Array[CandleSetting]
 @export var spacing := Vector3(1.0, 0.0, 0.0)
 
-var _candle_dict : Dictionary[int, CandleSetting] = {}
 var _instanced_candles : Array[FireBall] = []
 var _child_root_transform : Node3D
 
-func _ready():
-	for setting in candle_settings:
-		_candle_dict.set(setting.id, setting)
-	
+func _ready():	
 	setup_root()
 	
 	if not Engine.is_editor_hint():
@@ -22,12 +17,12 @@ func setup_root():
 		_child_root_transform.name = "CandleRoot"
 		add_child(_child_root_transform)
 	
-	_child_root_transform.position = -spacing * (len(candle_settings) - 1) * .5
+	_child_root_transform.position = -spacing * (CandleManager.get_candle_count() - 1) * .5
 	
 
 func refresh(sequence : Array[int]):
 	for i in range(len(_instanced_candles), len(sequence)):
-		var setting = _candle_dict.get(sequence[i]) as CandleSetting
+		var setting = CandleManager.get_candle(sequence[i])
 		var instance = setting.prefab.instantiate()
 		_child_root_transform.add_child(instance)
 		instance.position = spacing * i
